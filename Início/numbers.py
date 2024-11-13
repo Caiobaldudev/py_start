9b600514e53650738c6fb5bb86dac1066870a276
Caio Balduino <caiobalduinodev@hotmail.com>
2024-11-13 18:04:19 -0300

    numbers

---
+++ Início/numbers.py
@@ -0,0 +1,11 @@
+#Inteiro
+numero_inteiro = 3
+print("inteiro = ", numero_inteiro)
+
+#Real com ponto flutuante
+numero_real = 3.14
+print("Real com ponto flutuante = ", numero_real)
+
+#type()
+print("O tipo da variável e = ", type(numero_inteiro))
+print("O tipo da variável e = ", type(numero_real))
