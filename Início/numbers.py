5d4c16cfd8ce4e53a777a7742064307ec1a30465
Caio Balduino <caiobalduinodev@hotmail.com>
2024-11-14 17:10:03 -0300

    numbers conclusion

--- Início/numbers.py
+++ Início/numbers.py
@@ -9,3 +9,25 @@ print("Real com ponto flutuante = ", numero_real)
 #type()
 print("O tipo da variável e = ", type(numero_inteiro))
 print("O tipo da variável e = ", type(numero_real))
+
+#soma
+soma = 1 + 1
+print(soma)
+print("Transoformei em float: ", float(soma))
+#subtração
+subtracao = 1 - 1
+print(subtracao)
+#multiplição
+multiplicacao = 2 * 2
+print(multiplicacao)
+#divisao
+divisao = 12 / 2
+print(divisao)
+print("O tipo : ", type(divisao))
+print("Agora virou um inteiro: ", int(divisao))
+#modulo
+modulo = 5 % 2
+print(modulo)
+
+
+
