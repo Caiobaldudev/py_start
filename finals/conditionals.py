bca88ec4cbe8e3e9f2c8043e7978f3410fcbcad2
Caio Balduino <caiobalduinodev@hotmail.com>
2024-11-16 17:44:07 -0300

    conditionals

---
+++ finals/conditionals.py
@@ -0,0 +1,15 @@
+#Condicionais
+#if, elif e else
+idade = 18
+if idade >= 18:
+  print("Pode dirigir.")
+else:
+  print("Não pode dirigir.")
+
+if idade >= 18:
+  print("Você tem a maioridade!")
+elif idade >= 12:
+  print("Você é um adolescente!")
+else:
+  print("Você é muito novo!!")
+
