ad920f139d00be4fc4f85d69421600fd3792c8e4
Caio Balduino <caiobalduinodev@hotmail.com>
2024-11-16 15:41:18 -0300

    tuplas / tuple

---
+++ Início/tuple.py
@@ -0,0 +1,13 @@
+# Criando tupla
+tuple = (1, 2, 2, 3, 4)
+
+print('essa é a a tupla', tuple)
+print(tuple[0])
+
+#Método Count
+count = tuple.count(2)
+print('quantidade de vezes que o numero 2 aparece: ', count)
+
+#Método Index
+index = tuple.index(2)
+print('indice da primeira ocorrência do numero 2: ', index)
